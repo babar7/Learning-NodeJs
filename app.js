e26f78d22d0ea9a78ae0c2b216479414d7b617f2
Babar Shair <babarshair97@gmail.com>
2022-10-22 18:08:38 +0500

    day1: init setup

---
+++ app.js
@@ -0,0 +1,14 @@
+// function greet() {
+//   console.log("hello world !");
+// }
+
+// // functions are first-class
+// function logGreeting(cb) {
+//   cb();
+// }
+
+// logGreeting(greet);
+
+// Module
+var a = require("./greet");
+a.greet();
